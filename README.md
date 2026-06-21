a87681681d59af98674ff80e250d6b196df596fd
SpaceSettler <SS@DESKTOP-IULA8OP>
2026-06-21 00:30:23 -0600

    Updated read me

--- README.md
+++ README.md
@@ -1,32 +1,27 @@
 # Overview
 
-{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+This is a simple game that I originally thought of in my first years of high school. It involves simple avoiding lines that fall from the sky.
+The starting point of the lines and the speed progressively go up and get faster respectively. It a simple game that shows the ease of use of
+ggez, the main libray used.
 
-{Provide a description of the software that you wrote to demonstrate the Rust language.}
-
-{Describe your purpose for writing this software.}
-
-{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
-
-[Software Demo Video](http://youtube.link.goes.here)
+[Software Demo Video](https://youtu.be/RQ6YIUWqQXI)
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
+I used the Visual Studio IDE for the entire creation of the game.
 
-{Describe the programming language that you used and any libraries.}
+Rust is a C/C++ like language, but with more memory managmaent and a focus on being easy to use. Ggez was the main library used in creating the
+software. It includes many helpful tools to create 2d (and 3d) game components.
 
 # Useful Websites
 
 {Make a list of websites that you found helpful in this project}
 
-- [Web Site Name](http://url.link.goes.here)
-- [Web Site Name](http://url.link.goes.here)
+- [Rust](https://rust-lang.org/)
+- [ggez](https://ggez.rs/)
 
 # Future Work
 
-{Make a list of things that you need to fix, improve, and add in the future.}
-
-- Item 1
-- Item 2
-- Item 3
+I would like to add more visual clarity to the game. At the moment it is very bland, not hard to follow, but uninteresting to look at. I would
+also like to add more challenges too, like a shrinkning side to side movement room, etc. I am not aware of any things that need fixing. It is
+simple enough ot not have any, but like I mentioned there are many thing I would like to improve.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::VecDeque;
-
 use oorandom::Rand32;
 use getrandom;
 use ggez::{
@@ -10,6 +9,7 @@ use ggez::{
 };
 use winit::keyboard::{Key, NamedKey};
 
+//collection of constants for ease of use on sizing.
 const GRID_SIZE: (i16, i16) = (16, 8);
 
 const GRID_CELL_SIZE: (i16, i16) = (32, 32);
@@ -19,8 +19,10 @@ const SCREEN_SIZE: (f32, f32) = (
     GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
 );
 
+//make sure things run orderly at a constant rate
 const DESIRED_FPS: u32 = 64;
 
+//first struct is just essetnially a coordiante with function to generate coords, and convert.
 #[derive(Clone, Copy, PartialEq)]
 struct GridPos {
     x: i16,
@@ -59,6 +61,7 @@ impl From<(i16, i16)> for GridPos {
     }
 }
 
+//enum for directions able to mov,e and translate from raw keypress into usable names
 #[derive(Copy, Clone, PartialEq)]
 enum Move {
     Left,
@@ -77,6 +80,7 @@ impl Move {
     }
 }
 
+//obstable to ovoid by player. Made of a position on the grid, a vector of itself, and other variables to check conditions
 struct Obstacle {
     pos: GridPos,
     body: VecDeque<GridPos>,
@@ -88,8 +92,10 @@ struct Obstacle {
 
 impl Obstacle {
     pub fn new(pos: GridPos, timer: i32) -> Self {
+        //create the vector deque and add the first part of the line
         let mut body = VecDeque::new();
         body.push_back(GridPos::new(pos.x, pos.y).into());
+
         Obstacle {
             pos,
             body,
@@ -101,6 +107,8 @@ impl Obstacle {
     }
 
     fn update(&mut self) {
+        //a counter runs each frame and when it hits a threshold (drop_time) it moves the line down closer to the player, then resets
+        //and starts over.
         if self.counter == self.drop_time {
              self.body.push_back(GridPos::new(self.pos.x, self.pos.y + self.increment).into());
              self.counter = 0;
@@ -108,6 +116,7 @@ impl Obstacle {
         } else {
             self.counter += 1;
         }
+        //once the lines reaches the bottom of screen we can remove it and start a new one
         if self.pos.y + self.increment > 8 {
             self.increment = 1;
             self.end_reached = true;
@@ -115,7 +124,10 @@ impl Obstacle {
     }
 
     fn draw(&mut self, canvas: &mut graphics::Canvas) {
+        //draws each piece of the obstacle by using the from helper function defined in GridPos to convert to usable rectangle data for ggez drawing
         for seg in &self.body {
+            //redraws the whole line in the background color, effectively erasing it, if the line has reached the bottom of screen. Otherwise draws
+            //the line normally
             if self.end_reached {
                 canvas.draw(
                     &graphics::Quad,
@@ -136,11 +148,10 @@ impl Obstacle {
     }
 }
 
+//the player also has a GridPos but this time only tracks their movement and whether or not they hit the obstacle
 struct Player {
     pos: GridPos,
     dir: Move,
-    last_update_dir: Move,
-    next_dir: Option<Move>,
     is_dead: bool,
 }
 
@@ -149,22 +160,19 @@ impl Player {
         Player {
             pos,
             dir: Move::None,
-            last_update_dir: Move::None,
-            next_dir: None,
             is_dead: false,
         }
     }
 
+    //checks for collision between the bottom of the line and the player
      fn hits(&self, obstacle: &Obstacle) -> bool {
         self.pos.x == obstacle.pos.x && self.pos.y == obstacle.pos.y + obstacle.increment
     }
 
     fn update(&mut self, obstacle: &Obstacle) {
-        if self.last_update_dir == self.dir && self.next_dir.is_some() {
-            self.dir = self.next_dir.unwrap();
-            self.next_dir = None;
-        }
 
+        //if the key pressed is aligned with a directional movement defined in the move enum then we check and see if the player can move the
+        //way they want to and then move them there if posible.
         match self.dir {
             Move::Left => {
                 if self.pos.x > 0 {
@@ -181,14 +189,13 @@ impl Player {
             Move::None => (),
         }
 
+        //did they hit the obstacle? If yes then they are dead.
         if self.hits(obstacle) {
            self.is_dead = true;
         } 
-
-        self.last_update_dir = self.dir;
-
     }
 
+    //same draw as the obstacle, but no deque to loop through
     fn draw(&self, canvas: &mut graphics::Canvas) {
         let color = [0.0, 0.0, 1.0, 1.0];
         canvas.draw(
@@ -201,6 +208,8 @@ impl Player {
 }
 
 
+//the game state kinda holds most things in the game, so it has the player and the obstacle, plus some variables for checking things, and
+//difficulty manipulation
 struct GameState {
     player: Player,
     obstacle: Obstacle,
@@ -213,11 +222,15 @@ struct GameState {
 
 impl GameState {
     pub fn new() -> Self {
+        //set the player in the middle of the screen
         let player_pos = (8, 7).into();
+        //set the starting hight to 7
         let dif_multiplier = 7;
+        //use random librarys to generate seeds to randomize the placement of the lines
         let mut seed: [u8; 8] = [0; 8];
         getrandom::fill(&mut seed[..]).expect("Could not create RNG seed");
         let mut rng = Rand32::new(u64::from_ne_bytes(seed));
+        //start line at a random spot as defined by the seed
         let obstacle_pos = GridPos::random(&mut rng, GRID_SIZE.0, GRID_SIZE.1-dif_multiplier);
 
         GameState {
@@ -234,16 +247,20 @@ impl GameState {
 
 impl event::EventHandler for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
+        //only update at the desired rate
         while ctx.time.check_update_time(DESIRED_FPS) {
             if !self.gameover {
+                //call update in player with reference to obstacle so we can see if they are hit by it. If they are end the game
                 self.player.update(&self.obstacle);
                 if self.player.is_dead {
                     self.gameover = true;
                 }
                 self.obstacle.update();
                 if self.obstacle.end_reached {
+                    //if the obstacle reaches the end make a new one somewhere else randomly
                     let new_obstacle_pos = GridPos::random(&mut self.rng, GRID_SIZE.0, GRID_SIZE.1-self.dif_multiplier);
-                    self.obstacle = Obstacle::new(new_obstacle_pos, self.timer);  
+                    self.obstacle = Obstacle::new(new_obstacle_pos, self.timer);
+                    //increase the score, and every ten times either lower the height of lines, or increase their speed
                     self.score += 1;
                     if self.score % 10 == 0 && self.timer != 2 && self.dif_multiplier != 3 {
                         if self.score % 20 == 0 {
@@ -259,19 +276,23 @@ impl event::EventHandler for GameState {
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        //draw the background on solid color, the same color will be used to erase old lines
         let mut canvas =
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.0, 1.0, 0.0, 1.0]));
 
-        
+        //draw calls for both player and obstacle
         self.player.draw(&mut canvas);
         self.obstacle.draw(&mut canvas);
       
+        //canvas finish will actaully draw everything it has queued up
         canvas.finish(ctx)?;
         
+        //yeilds control and gives back to system because of fps implementation
         ggez::timer::yield_now();
         Ok(())
     }
     
+    //if a key is pressed get its data, and attempt to translate via enum function.
     fn key_down_event(&mut self, ctx: &mut Context, input: KeyInput, _repeat: bool) -> GameResult {
         if let Some(dir) = Move::from_key(&input.event.logical_key) {
             self.player.dir = dir;
@@ -283,11 +304,13 @@ impl event::EventHandler for GameState {
 }
 
 fn main() -> GameResult {
+    //generic setup
     let (ctx, events_loop) = ggez::ContextBuilder::new("Line Dodge", "SpaceSettler")
         .window_setup(ggez::conf::WindowSetup::default().title("LineDodge!"))
         .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
         .build()?;
         
     let state = GameState::new();
+    //actually runs the whole thing by calling everything built and the running in event loop
     event::run(ctx, events_loop, state)
 }
